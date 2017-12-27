15970d22adbf32cb57eeaafa19ab6d0483cf440b
unknown <wxliny@ZZYT.com>
2017-12-27 16:44:41 +0800

    first commit

---
+++ outFile/bundle.js
@@ -0,0 +1,12 @@
+webpackJsonp([0],{
+
+/***/ 31:
+/***/ (function(module, exports, __webpack_require__) {
+
+(function webpackMissingModule() { throw new Error("Cannot find module \"../src/css/style.scss\""); }());
+(function webpackMissingModule() { throw new Error("Cannot find module \"../src/main.js\""); }());
+
+
+/***/ })
+
+},[31]);
